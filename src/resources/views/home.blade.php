1ee698cda79c1cae5da5a8583b5f553ba3c961fe
Ubuntu <ubuntu@ip-172-31-30-27.ec2.internal>
2020-12-15 03:14:17 +0000

    merapikan desain view laravel

--- src/resources/views/home.blade.php
+++ src/resources/views/home.blade.php
@@ -2,61 +2,55 @@
 
 @section('content')
     <div class="container">
-     <a href="" class="btn btn-success float-right mb-4"><i class="fas fa-plus"></i> Buat Kelas Baru</a>
+     <a href="{{ route("nama.form") }}" class="btn btn-success float-right mb-4"><i class="fas fa-plus"></i> Buat Daftar Baru</a>
         <div class="clearfix"></div>
 
         <div class="row">
-            
-                <div class="col-md-3">
-                     <div class="card">
-        <div class="card-header"></div>
-        <div class="card-header bg-primary text-white">
-        <h5>PMK UNIVERSITAS IBBI</H5>
-         </div>
-     <div class="card-body">
-     Melalui situs website ini di harapkan para mahasiswa dan mahasiswi mengetahui dengan jelas mudah kegiatan dan jadwal kegiatan PMK UNIVERSITAS IBBI</div>
-     <div class="card-footer">     
-        <a href="#"class ="btn-danger float-right"><i class="fas fa-trash"></i>Hapus</a>
+            <div class="col-md-4">
+                <div class="card-header bg-primary text-white">
+               <center> <h5>PMK UNIVERSITAS IBBI</H5></center>
+                </div>
+                    <div class="card-body">
+                    Situs website PMK IBBI dapat membantu menyimpan biodata kepengurusan organisasi PMK UNIVERSITAS IBBI
+                    </div>
+                        <div class="card-footer">     
+                        <a href="#"class ="btn-danger float-right"><i class="fas fa-trash"></i>Hapus</a>
+                        </div>
+            </div>
+
+            <div class="col-md-4">
+                <div class="card-header bg-primary text-white">
+                <center><h5>PMK UNIVERSITAS IBBI</H5></center>
+                </div>
+                    <div class="card-body">
+                    Dan melalui situs ini juga dapat menyimpan ataupun sebagai sarana penyimpanan kegiatan PMK UNIVERSITAS IBBI
+                    </div>
+                        <div class="card-footer">     
+                        <a href="#"class ="btn-danger float-right"><i class="fas fa-trash"></i>Hapus</a>
+                        </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card-header bg-primary text-white">
+                <center><h5>PMK UNIVERSITAS IBBI</H5></center>
+                </div>
+                    <div class="card-body">
+                     Situs website ini juga bertujuan mempermudah pengurus Organisasi IBBI dalam pengisian biodata
+                    </div>
+                        <div class="card-footer">     
+                        <a href="#"class ="btn-danger float-right"><i class="fas fa-trash"></i>Hapus</a>
+                        </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card-header bg-primary text-white">
+                <center><h5>PMK UNIVERSITAS IBBI</H5></center>
+                </div>
+                    <div class="card-body">
+                    Melalui situs website ini di harapkan dapat mempermudah kegiatan organisasi PMK UNIVERSITAS IBBI
+                    </div>
+                        <div class="card-footer">     
+                        <a href="#"class ="btn-danger float-right"><i class="fas fa-trash"></i>Hapus</a>
+                        </div>
+            </div>
         </div>
-     </div>
-     </div>
-     <div class="col-md-3 ">
-     <div class="card">
-        <div class="card-header"></div>
-        <div class="card-header bg-primary text-white">
-        <h5>PMK UNIVERSITAS IBBI</H5>
-         </div>
-     <div class="card-body">
-     Dan di situs ini juga para mahasiswa dan mahasiswi dapat melihat picture kegiatan PMK yang telah di lakukan</div>
-     <div class="card-footer">     
-        <a href="#"class ="btn-danger float-right"><i class="fas fa-trash"></i>Hapus</a>
-        
-
- </div>
-     </div>
-     </div>
-     <div class="col-md-3 ">
-     <div class="card">
-        <div class="card-header"></div>
-        <div class="card-header bg-primary text-white">
-        <h5>PMK UNIVERSITAS IBBI</H5>
-         </div>
-     <div class="card-body">
-     PMK juga memiliki beberapa kegiatan yang dapat mahasiswa/mahaisiswi ikuti </div>
-     <div class="card-footer">     
-        <a href="#"class ="btn-danger float-right"><i class="fas fa-trash"></i>Hapus</a>
-
- </div>
-     </div>
-     </div>
-     <div class="col-md-3 ">
-     <div class="card">
-        <div class="card-header"></div>
-        <div class="card-header bg-primary text-white">
-        <h5>PMK UNIVERSITAS IBBI</H5>
-         </div>
-     <div class="card-body">
-     Dan di situs ini juga akan di muat beberapa picture kegiatan PMK yang sudah di lakukan</div>
-     <div class="card-footer">     
-        <a href="#"class ="btn-danger float-right"><i class="fas fa-trash"></i>Hapus</a>
+           
 @endsection
