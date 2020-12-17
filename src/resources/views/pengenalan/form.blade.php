732027e266340d9471d1e8aa55d53ac2f5733821
Ubuntu <ubuntu@ip-172-31-30-27.ec2.internal>
2020-12-17 09:27:54 +0000

    perbaiki

--- src/resources/views/pengenalan/form.blade.php
+++ src/resources/views/pengenalan/form.blade.php
@@ -1,31 +1,6 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <nav class="navbar navbar-expand-lg navbar-light bg-primary mb-4">
-        <a class="navbar-brand" href="#"></a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarText">
-          <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-              <a class="nav-link text-white" href="home">BERANDA <span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link text-white" href="formnama">NAMA PENGURUS</a>
-              </li>
-            <li class="nav-item">
-                <a class="nav-link text-white" href="tentang">JABATAN</a>
-              </li>
-                
-            <li class="nav-item">
-              <a class="nav-link text-white" href="tentang">KEGIATAN</a>
-            </li>
-          </ul>
-        </div>
-    </nav>
-</div>
     <div class="container">
         <form action="{{ route("pengenalan.create") }}" method="POST">   
             @csrf
